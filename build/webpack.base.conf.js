4bfbcb7be7fe56afead161b2854fef8d081cba1c
Egor Batrakov <goshanprm@gmail.com>
2019-12-25 21:35:16 +0300

    prepare template for PUG

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,8 +10,8 @@ const PATHS = {
   assets: 'assets/'
 }
 
-const PAGES_DIR = PATHS.src
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'))
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
 module.exports = {
   externals: {
@@ -39,6 +39,9 @@ module.exports = {
   },
   module: {
     rules: [{
+      test: /\.pug$/,
+      loader: 'pug-loader',
+    }, {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: '/node_modules/'
@@ -101,7 +104,7 @@ module.exports = {
     ]),
     ...PAGES.map(page => new HtmlWebpackPlugin({
       template: `${PAGES_DIR}/${page}`,
-      filename: `./${page}`
+      filename: `./${page.replace(/\.pug/,'.html')}`
     }))
   ],
 }
