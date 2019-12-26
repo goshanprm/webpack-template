06e0aa67294051615211de36bdb89d0e4477cb9e
Egor Batrakov <goshanprm@gmail.com>
2019-12-26 18:45:55 +0300

    refactor(webpack.config.js) fix production and development modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const fs = require('fs')
+const merge = require('webpack-merge');
 const pug = require('./webpack/pug');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const devconfig = require('./webpack/dev.config');
 
 const PATHS = {
   src: path.join(__dirname, './src'),
@@ -14,101 +16,110 @@ const PATHS = {
 const PAGES_DIR = `${PATHS.src}/pug/pages/`
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
-module.exports = {
-  externals: {
-    paths: PATHS
-  },
-  entry: {
-    app: PATHS.src,
-  },
-  output: {
-    filename: `${PATHS.assets}js/[name].[hash].js`,
-    path: PATHS.dist,
-    publicPath: '/'
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          name: 'vendors',
-          test: /node_modules/,
-          chunks: 'all',
-          enforce: true
+const common = merge([
+  {
+    externals: {
+      paths: PATHS
+    },
+    entry: {
+      app: PATHS.src,
+    },
+    output: {
+      filename: `${PATHS.assets}js/[name].[hash].js`,
+      path: PATHS.dist,
+      publicPath: '/'
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            name: 'vendors',
+            test: /node_modules/,
+            chunks: 'all',
+            enforce: true
+          }
         }
       }
-    }
-  },
-  module: {
-    rules: [{
-      test: /\.pug$/,
-      loader: 'pug-loader',
-      options: {
-        pretty: true
-      }
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
-    }, {
-      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.sass$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
-        }, {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }, {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
         }
-      ]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+      }, {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
         }
-      ]
-    }]
+      }, {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+          }
+        ]
+      }]
+    },
+    resolve: {
+      alias: {
+        '~': PATHS.src,
+      }
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `${PATHS.assets}css/[name].[hash].css`,
+      }),
+      new CopyWebpackPlugin([
+        { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+        { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+        { from: `${PATHS.src}/static`, to: '' },
+      ]),
+      ...PAGES.map(page => new HtmlWebpackPlugin({
+        template: `${PAGES_DIR}/${page}`,
+        filename: `./${page.replace(/\.pug/,'.html')}`
+      }))
+    ],
   },
-  resolve: {
-    alias: {
-      '~': PATHS.src,
-    }
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[hash].css`,
-    }),
-    new CopyWebpackPlugin([
-      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
-      { from: `${PATHS.src}/static`, to: '' },
-    ]),
-    ...PAGES.map(page => new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/${page}`,
-      filename: `./${page.replace(/\.pug/,'.html')}`
-    }))
-  ],
-}
+  pug(),
+]);
+
+module.exports = function(env) {
+  if (env === 'production') {
+    return common;
+  }
+  if (env === 'development') {
+    return merge([
+      common,
+      devconfig(),
+    ]);
+  }
+};
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,26 +1,19 @@
 const webpack =  require('webpack')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('../webpack.config')
 
-const devWebpackConfig = merge(baseWebpackConfig, {
-  // DEV config
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: baseWebpackConfig.externals.paths.dist,
-    port: 8081,
-    overlay: {
-      warnings: true,
-      errors: true
-    }
-  },
-  plugins: [
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map'
-    })
-  ]
-})
-
-module.exports = new Promise((resolve, reject) => {
-  resolve(devWebpackConfig)
-})
+module.exports = function() {
+  return {
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      port: 8081,
+      overlay: {
+        warnings: true,
+        errors: true
+      }
+    },
+    plugins: [
+      new webpack.SourceMapDevToolPlugin({
+        filename: '[file].map'
+      })
+    ]
+  };
+};
